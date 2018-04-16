9f8bad119fb457e8c05b92ed547683740cb777a5
AvelJonathan <jonathan.avel8@gmail.com>
2018-04-16 14:45:41 +0200

    Add Authentification

--- WalkingDead.js
+++ WalkingDead.js
@@ -1,35 +1,112 @@
-// Initialize Firebase
 var config = {
     apiKey: "AIzaSyCJwHz6zimEk9qZnI43wpzxcRSBiQqPt5I",
     authDomain: "walking-dead-6b01a.firebaseapp.com",
     databaseURL: "https://walking-dead-6b01a.firebaseio.com",
     projectId: "walking-dead-6b01a",
     storageBucket: "walking-dead-6b01a.appspot.com",
-    messagingSenderId: "988412893912"
+    messagingSenderId: "988412893912",
 };
 firebase.initializeApp(config);
-
-// Get a reference to the database service
-var database = firebase.database();
-
-var character = firebase.database().ref('Characters/');
+var db = firebase.database();
+var character = db.ref('Characters');
 
 character.once('value', function (snapshot) {
     snapshot.forEach(function (childSnapshot) {
         var childData = childSnapshot.val();
-        console.log(childData);
-        document.getElementById('liste_char').innerHTML += '<li>' + childData.name + '</li>';
+        if (childData.uid === myId || !childData.uid) {
+            document.getElementById('character-list').innerHTML += '<li>' + childData.name +
+                '</li>';
+        }
     });
 });
 
 function writeUserData(Name) {
-    firebase.database().ref('Characters/' + name).push({
+    db.ref('Characters/' + name).push({
         name: Name,
+        uid: myId,
     });
 
 }
-function AddChar() {
+
+function PushOnFirebase() {
     let name = document.getElementById("Name").value;
     writeUserData(name);
     window.location.reload();
+}
+
+
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: 'index.html',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: 'http://localhost:8080/cgu'
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+myId = '';
+
+function initApp() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        document.getElementById('showUser').style.display = 'none';
+
+        if (user) {
+            document.getElementById('showUser').style.display = 'block';
+            // All datas
+            // User is signed in.
+            const displayName = user.displayName;
+            const email = user.email;
+            const emailVerified = user.emailVerified;
+            const photoURL = user.photoURL;
+            const uid = user.uid;
+            const phoneNumber = user.phoneNumber;
+            const providerData = user.providerData;
+
+
+            // retour de l'utilisateur après authentification
+            user.getIdToken().then((accessToken) => {
+                document.getElementById('sign-in-status').textContent = 'Signed in';
+                document.getElementById('sign-in').textContent = 'Sign out';
+                document.getElementById('account-details').textContent = JSON.stringify({
+                    displayName: displayName,
+                    email: email,
+                    emailVerified: emailVerified,
+                    phoneNumber: phoneNumber,
+                    photoURL: photoURL,
+                    uid: uid,
+                    accessToken: accessToken,
+                    providerData: providerData
+                }, null, ' ');
+            });
+
+            myId = user.uid;
+        } else {
+
+            // Gestion de la deconnexion
+
+            document.getElementById('sign-in-status').textContent = 'Signed out';
+            document.getElementById('sign-in').textContent = 'Sign in';
+            document.getElementById('account-details').textContent = 'null';
+        }
+    }, (error) => { // gestion de erreur de connexion
+        console.error(error);
+    });
+}
+initApp();
+
+
+function logOut() {
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+    }).catch(function (error) {
+        // An error happened.
+    });
 }
